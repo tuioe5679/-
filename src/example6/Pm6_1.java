c2e90942a76baab013f021467bcc1d3cce5d15b4
tuioe <qodlgks123@naver.com>
2022-05-09 12:42:04 +0900

    백준 단계별 6단계

---
+++ src/example6/Pm6_1.java
@@ -0,0 +1,13 @@
+package example6;
+
+import java.util.Scanner;
+
+public class Pm6_1 {
+
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+		char a = stdin.next().charAt(0);
+		int i = (int)a;
+		System.out.println(i);
+	}
+}
---
+++ src/example6/Pm6_3.java
@@ -0,0 +1,9 @@
+package example6;
+
+public class Pm6_3 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
